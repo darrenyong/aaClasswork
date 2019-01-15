2bc9264cc91680d18505102f4dd1c48c4a9196fb
darrenyong <contact.darrenyong@gmail.com>
2019-01-14 19:33:03 -0800

    Included #initialize for Employee and Manager classes

--- W2D1/class_inheritance.rb
+++ W2D1/class_inheritance.rb
@@ -1,7 +1,20 @@
 class Employee
+  attr_reader :name, :title, :salary, :boss
+
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
 
 end
 
 class Manager < Employee
+
+  def initialize(name, title, salary, boss, employees)
+    super(name, title, salary, boss)
+    @employees = employees
+  end
 
 end

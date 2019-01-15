60268abd4bb7fb5c51316e099e21684254847677
darrenyong <contact.darrenyong@gmail.com>
2019-01-14 19:33:49 -0800

    Added #bonus methods for both Employee and Manager classes

--- W2D1/class_inheritance.rb
+++ W2D1/class_inheritance.rb
@@ -8,6 +8,10 @@ class Employee
     @boss = boss
   end
 
+  def bonus(multiplier)
+    return salary * multiplier
+  end
+
 end
 
 class Manager < Employee
@@ -15,6 +19,14 @@ class Manager < Employee
   def initialize(name, title, salary, boss, employees)
     super(name, title, salary, boss)
     @employees = employees
+  end
+
+  def bonus(multiplier)
+    total = 0
+    @employees.each do |emp|
+      total += emp.salary
+    end
+    return total * multiplier
   end
 
 end

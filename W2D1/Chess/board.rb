1089ea5cd70c3ef82e5b0dbe945a560eddd582c7
darrenyong <contact.darrenyong@gmail.com>
2019-01-15 18:51:45 -0800

    Added Board#fill_board to fill board with Piece and NullPiece instances

--- W2D1/Chess/board.rb
+++ W2D1/Chess/board.rb
@@ -6,8 +6,28 @@ class Board
   def initialize
     @chess_board = Array.new(8) {Array.new(8)}
     @sentinel = NullPiece.instance
+    fill_board
   end
 
+  def fill_board
+    @chess_board.each_index do |idx1|
+      @chess_board.each_index do |idx2|
+        if idx1 == 0 || idx1 == 1 || idx1 == 6 || idx1 == 7
+          @chess_board[idx1][idx2] = Piece.new([idx1, idx2])
+        else
+          @chess_board[idx1][idx2] = sentinel
+        end
+      end
+    end
+  end
+
+  def [](pos)
+    @chess_board[pos[0]][pos[1]]
+  end
+
+  def []=(pos, value)
+    @chess_board[pos[0]][pos[1]] = value
+  end
 
 end
 

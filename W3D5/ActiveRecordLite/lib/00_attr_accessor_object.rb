5852a9c2137bc95df8aaeb31e80f02ab44f348a1
darrenyong <contact.darrenyong@gmail.com>
2019-01-25 15:33:09 -0800

    Implemented macro getter and setter methods

--- W3D5/ActiveRecordLite/lib/00_attr_accessor_object.rb
+++ W3D5/ActiveRecordLite/lib/00_attr_accessor_object.rb
@@ -1,7 +1,9 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
-
+    names.each do |name|
+      self.class_eval("def #{name};@#{name};end")
+      self.class_eval("def #{name}=(val); @#{name} = val; end")
     end
   end
 end

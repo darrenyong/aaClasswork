358de408ca4cd871fce199968a4b75bbe6834012
darrenyong <contact.darrenyong@gmail.com>
2019-01-31 12:09:29 -0800

    Added HTML Beautifier gem for ERB styling

--- W4D4/MusicApp/app/controllers/users_controller.rb
+++ W4D4/MusicApp/app/controllers/users_controller.rb
@@ -7,11 +7,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save!
+    render json: @user
   end
 
 
   private
   def user_params
-    params.require[:user].permit[:email, :password]
+    params.require(:user).permit(:email, :password)
   end
 end

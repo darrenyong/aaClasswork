683c2ed390a7596dfc2e626e0bd79e9e4d2c61f4
darrenyong <contact.darrenyong@gmail.com>
2019-01-31 14:12:08 -0800

    Updated SessionsController#create to log users in and show errors if invalid credentials

--- W4D4/MusicApp/app/controllers/sessions_controller.rb
+++ W4D4/MusicApp/app/controllers/sessions_controller.rb
@@ -1,13 +1,21 @@
 class SessionsController < ApplicationController
 
   def new
-
   end
 
   def create
+    user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if user
+      login!(user)
+      render json: user
+    else
+      flash[:errors] = ["Invalid credentials"]
+      redirect_to new_session_url
+    end
   end
 
   def destroy
+
   end
 
 end

72042f2f9a920f98dde6a160217def5e77f5bc3b
darrenyong <contact.darrenyong@gmail.com>
2019-01-31 14:10:40 -0800

    Added #login!, #logged_in?, #logout! and #current_user in ApplicationController

--- W4D4/MusicApp/app/controllers/application_controller.rb
+++ W4D4/MusicApp/app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  helper_method :require_login, :loggedin?
+
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout!
+    current_user.reset_session_token
+    user.session_token = nil
+  end
+
+  def current_user
+    @current_user = User.find_by(session_token: session[:session_token])
+  end
+
+  def require_login
+    redirect_to new_session_url if current_user.nil?
+  end
+
 end
